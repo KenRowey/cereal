4062a6788cfd51210e03f40eee66ef90868a7185
Ken Rowey <ken.rowey@gmail.com>
2025-01-24 20:42:51 +0000

    updated test

---
+++ tests/test_pantry.rb
@@ -0,0 +1,20 @@
+require 'minitest/autorun'
+require_relative '../pantry'
+require_relative '../cereal'
+
+class TestPantry < Minitest::Test
+  def setup
+    @pantry = Pantry.new
+  end
+
+  def test_add_cereal
+    cereal = Cereal.new("cheerios", 12, 110)
+    @pantry.instance_variable_get(:@cereals) << cereal
+
+    cereals = @pantry.instance_variable_get(:@cereals)
+    assert_equal 1, cereals.size
+    assert_equal "cheerios", cereals[0].name
+    assert_equal 12, cereals[0].ounces
+    assert_equal 110, cereals[0].calories
+  end
+end
